7aa7edc6decfb1b3e53b02790b512c203298a907
minkikim98 <minkikim98@gmail.com>
2021-02-03 10:49:03 -0800

    finished graph intro project

--- problems/graph.js
+++ problems/graph.js
@@ -1,33 +1,66 @@
 
 class Graph {
   constructor() {
-    // Code goes here ...
+    this.adjList = {};
   }
 
   addVertex(vertex) {
-    // Code goes here ...
+    if (!this.adjList[vertex]) {
+      this.adjList[vertex] = [];
+    }
   }
 
   addEdges(srcValue, destValue) {
-    // Code goes here ...
+    if (!this.adjList[srcValue]) this.addVertex(srcValue);
+    if (!this.adjList[destValue]) this.addVertex(destValue);
+
+    this.adjList[srcValue].push(destValue);
+    this.adjList[destValue].push(srcValue);
   }
 
   buildGraph(edges) {
-    // Code goes here ...
+    edges.forEach(subArr => {
+      if (subArr.length === 1) this.addVertex(subArr[0]);
+      else this.addEdges(...subArr);
+    });
+    return this.adjList;
   }
 
   breadthFirstTraversal(startingVertex) {
-    // Code goes here ...
+    let resultArr = [startingVertex];
+    let Q = [startingVertex];
+    while (Q.length > 0) {
+      let first = Q.shift();
+      if (!resultArr.includes(first)) resultArr.push(first);
+      this.adjList[first].forEach(el => {
+        if (!resultArr.includes(el)) Q.push(el);
+      });
+    }
+    return resultArr;
   }
 
   depthFirstTraversalIterative(startingVertex) {
-    // Code goes here ...
+    let stack = [startingVertex];
+    let resultArr = [startingVertex];
+    while (stack.length > 0) {
+      let last = stack.pop();
+      if (!resultArr.includes(last)) resultArr.push(last);
+      this.adjList[last].forEach(el => {
+        if (!resultArr.includes(el)) stack.push(el);
+      });
+    }
+    return resultArr;
   }
 
   depthFirstTraversalRecursive(startingVertex, visited = new Set(), vertices = []) {
-    // Code goes here ...
+    if (visited.has(startingVertex)) return;
+    visited.add(startingVertex);
+    vertices.push(startingVertex);
+    this.adjList[startingVertex].forEach(vertex => {
+      this.depthFirstTraversalRecursive(vertex, visited, vertices);
+    });
+    return vertices;
   }
-
 }
 
 module.exports = {
